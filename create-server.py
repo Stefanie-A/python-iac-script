08aee33aa719f8a7728c8e56ae121a9d70aabe13
Stefanie-A <albertstephanie630@gamil.com>
2023-12-15 02:14:40 +0100

    create server

---
+++ create-server.py
@@ -0,0 +1,18 @@
+#Creating a server on digital ocean
+
+import digitalocean
+
+name = input("Please enter a name for your droplet: ")
+do_token = " "
+password = input("Please create a password for your server: ") 
+
+droplet = digitalocean.Droplet(token=do_token,
+    name= name,
+    region="nyc1",
+    image= "ubuntu-20-04-x64",
+    size_slug= "s-1vcpu-1gb", # 1GB RAM
+    ssh_password= password,
+    backups=True)
+
+if __name__ == "__main__":
+    droplet.create()
