1fb641533bebf4daaac46f971b47e0765f72c2d8
Stefanie-A <albertstephanie630@gamil.com>
2023-12-20 17:55:20 +0100

    List all droplet(s)

--- create-server.py
+++ create-server.py
@@ -3,7 +3,7 @@
 import digitalocean
 
 name = input("Please enter a name for your droplet: ")
-do_token = " "
+do_token = "dop_v1_0a5e40c6ae1674e7c35d89e2a5fdc1597602d0a3836d5e5f4fd7407bd0dd6fed"
 password = input("Please create a password for your server: ") 
 
 droplet = digitalocean.Droplet(token=do_token,
@@ -16,3 +16,10 @@ droplet = digitalocean.Droplet(token=do_token,
 
 if __name__ == "__main__":
     droplet.create()
+
+
+
+#Listing all droplets
+    
+manager = digitalocean.Manager(token=do_token)
+print(manager.get_all_droplets())
